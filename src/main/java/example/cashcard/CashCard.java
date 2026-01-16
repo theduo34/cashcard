c635d7a516c84ee953b07258fc7769ba2b77abe2
Emmanuel <emmsom506@gmail.com>
2026-01-16 14:20:31 +0000

    Add create cashcard endpoint

--- src/main/java/example/cashcard/CashCard.java
+++ src/main/java/example/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package example.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-public record CashCard(@Id Long Id, Double amount) {
+public record CashCard(@Id Long id, Double amount) {
 }
--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -2,7 +2,9 @@ package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -25,7 +27,16 @@ class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard() {
-        return null;
+    private ResponseEntity<Void> createCashCard(
+            @RequestBody CashCard request,
+            UriComponentsBuilder ucb
+    ) {
+        CashCard savedCashCard = cashCardRepository.save(request);
+        URI locationOfNewCashCard = ucb
+                .path("/cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
